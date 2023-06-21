d34a4661ed9db646606776d02620f69ab062e650
Yaroslav82 <yargay230607@gmail.com>
2023-06-21 14:27:45 +0300

    Add address info in getData

--- src/app/views/OrderAView.java
+++ src/app/views/OrderAView.java
@@ -1,4 +1,17 @@
 package app.views;
 
+import java.util.Scanner;
+
 public class OrderAView extends OrderView {
+
+    @Override
+    public String[] getData() {
+        String[] data = super.getData();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter store/office address: ");
+        String address = scanner.nextLine();
+
+        return new String[] {data[0], data[1], data[2], address};
+    }
 }
